e4bc47a58623e2852682ba541107d4ab8cf7c58b
David Larralde <larralde.david@gmail.com>
2020-12-21 22:47:33 +0100

    securité par is granted

--- src/Controller/EvaluationController.php
+++ src/Controller/EvaluationController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
